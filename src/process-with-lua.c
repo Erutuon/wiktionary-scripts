696f87238cca7ee5535339a64615c25c8c51ee22
gbertilson <5840197+Erutuon@users.noreply.github.com>
2019-04-16 16:53:13 -0500

    add input file and namespace parameters to process-with-lua

--- src/process-with-lua.c
+++ src/process-with-lua.c
@@ -6,6 +6,12 @@
 #include "utils/parser.h"
 #include "utils/string_slice.h"
 #include "utils/buffer.h"
+#include "commander.h"
+
+typedef struct {
+	FILE * input_file;
+	Wiktionary_namespace_t * namespaces;
+} option_t;
 
 static void print_Lua_error(lua_State * L, const char * msg) {
 	EPRINTF("%s", msg);
@@ -46,21 +52,120 @@ static bool process_page (parse_info * info) {
 	return res;
 }
 
-int main (int argc, const char * * argv) {
-	Wiktionary_namespace_t namespaces[] = {
-		NAMESPACE_RECONSTRUCTION,
-		NAMESPACE_APPENDIX,
-		NAMESPACE_NONE
-	};
-	lua_State * L;
+#define CHECK_FILE(file) \
+	if ((file) == NULL) \
+		perror("could not open file"), exit(-1)
+
+static inline void get_input_file (command_t * commands) {
+	option_t * options = commands->data;
+	FILE * input_file = fopen(commands->arg, "rb");
+	CHECK_FILE(input_file);
+	options->input_file = input_file;
+}
+
+#define MAX_NAMESPACES 16
+
+static inline void add_namespaces (command_t * commands) {
+	option_t * options = commands->data;
+	Wiktionary_namespace_t * namespaces = options->namespaces;
+	int characters_read = 0, total_characters_read = 0, i = 0;
+	Wiktionary_namespace_t namespace;
+	const char * arg = commands->arg;
 	
-	if (argc < 2)
-		CRASH_WITH_MSG("not enough arguments\n");
+	if (namespaces[0] != NAMESPACE_NONE)
+		CRASH_WITH_MSG("cannot supply namespaces twice");
 		
+	if (strcmp(arg, "all") == 0) // Search all namespaces.
+		return;
+		
+	while (sscanf(arg + total_characters_read,
+	              "%d%n",
+	              &namespace,
+	              &characters_read) == 1) {
+		total_characters_read += characters_read;
+		
+		if (i >= MAX_NAMESPACES - 1)
+			CRASH_WITH_MSG("too many namespaces; max %d\n", MAX_NAMESPACES);
+			
+		namespaces[i++] = namespace;
+	}
+	
+	namespaces[i] = NAMESPACE_NONE;
+	
+	size_t arg_len = strlen(arg);
+	
+	if ((size_t) total_characters_read != arg_len)
+		EPRINTF("Did not read all characters in list of namespaces in '%s'\n",
+		        commands->arg);
+}
+
+static inline char * process_args (option_t * options,
+                                   int argc,
+                                   char * * argv) {
+	command_t commands;
+	command_init(&commands, argv[0], "0");
+	commands.data = options;
+	char * script_file;
+	
+	command_option(&commands, "-i", "--input <file>",
+	               "XML page dump file",
+	               get_input_file);
+	               
+	command_option(&commands, "-n", "--namespaces <numbers>",
+	               "list of namespace numbers",
+	               add_namespaces);
+	               
+	command_parse(&commands, argc, argv);
+	
+	if (commands.argc == 1)
+		script_file = strdup(commands.argv[0]);
+	else if (commands.argc == 0)
+		CRASH_WITH_MSG("no script file given\n");
+	else if (commands.argc > 1)
+		CRASH_WITH_MSG("too many script files given\n");
+		
+	if (options->namespaces[0] == NAMESPACE_NONE) {
+		static Wiktionary_namespace_t default_namespaces[] = {
+			NAMESPACE_MAIN,
+			NAMESPACE_RECONSTRUCTION,
+			NAMESPACE_APPENDIX,
+			NAMESPACE_NONE
+		};
+		options->namespaces = default_namespaces;
+	}
+	
+	EPRINTF("searching in namespaces ");
+	
+	for (int i = 0; options->namespaces[i] != NAMESPACE_NONE; ++i) {
+		if (i > 0)
+			EPRINTF(", ");
+			
+		const char * name = get_namespace_name(options->namespaces[i]);
+		EPRINTF("%s", strlen(name) == 0 ?  "main" : name);
+	}
+	
+	putc('\n', stderr);
+	
+	command_free(&commands);
+	
+	return script_file;
+}
+
+int main (int argc, const char * * argv) {
+	option_t options = {0};
+	Wiktionary_namespace_t namespaces[MAX_NAMESPACES];
+	lua_State * L;
+	char * script_file;
+	
+	options.namespaces = namespaces;
+	options.namespaces[0] = NAMESPACE_NONE;
+	
+	script_file = process_args(&options, argc, argv);
+	
 	L = luaL_newstate();
 	luaL_openlibs(L);
 	
-	if (luaL_dofile(L, argv[1]) != LUA_OK) {
+	if (luaL_dofile(L, script_file) != LUA_OK) {
 		print_Lua_error(L, "error while running script");
 		exit(EXIT_FAILURE);
 	}
@@ -68,9 +173,16 @@ int main (int argc, const char * * argv) {
 	if (lua_type(L, -1) != LUA_TFUNCTION)
 		CRASH_WITH_MSG("script did not return a function");
 		
-	parse_Wiktionary_page_dump(stdin, process_page, namespaces, L);
-	
+	parse_Wiktionary_page_dump(options.input_file != NULL
+	                           ? options.input_file
+	                           : stdin,
+	                           process_page,
+	                           namespaces,
+	                           L);
+	                           
 	lua_close(L);
+	
+	free(script_file);
 	
 	return 0;
 }
