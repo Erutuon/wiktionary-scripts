31bfa053a1e20adfc96e46b83fa5b1d46da7f04a
gbertilson <5840197+Erutuon@users.noreply.github.com>
2019-05-04 01:13:17 -0500

    prevent iterate_templates from skipping first template on each page

--- lua/iterate_templates.lua
+++ lua/iterate_templates.lua
@@ -3,23 +3,17 @@ local parse_template = require "parse_template"
 return function (content, title_start, template_start)
 	title_start = title_start or '\1'
 	template_start = template_start or '\n'
-	local pattern = title_start .. '(.-)' .. template_start .. '(.-)%f[' .. title_start .. '\0]'
+	local pattern = title_start .. '(.-)(' .. template_start .. '.-)%f[' .. title_start .. '\0]'
 	
-	local function yielder ()
+	return coroutine.wrap(function ()
 		for title, text in content:gmatch(pattern) do
 			for pos in text:gmatch(template_start .. '(){{') do
 				local success, template = pcall(parse_template, text, pos)
 				
 				if success then
-					coroutine.yield(title, template)
+					coroutine.yield(template, title)
 				end
 			end
 		end
-	end
-	
-	local co = coroutine.create(yielder)
-	
-	return function ()
-		return select(2, coroutine.resume(co))
-	end
+	end)
 end
