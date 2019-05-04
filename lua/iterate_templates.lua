c24d5465101242b21ca8e224d696b20e62c374ac
gbertilson <5840197+Erutuon@users.noreply.github.com>
2019-05-04 01:13:40 -0500

    add iterate_links

--- lua/iterate_templates.lua
+++ lua/iterate_templates.lua
@@ -1,6 +1,6 @@
 local parse_template = require "parse_template"
 
-return function (content, title_start, template_start)
+local function iterate_templates(content, title_start, template_start)
 	title_start = title_start or '\1'
 	template_start = template_start or '\n'
 	local pattern = title_start .. '(.-)(' .. template_start .. '.-)%f[' .. title_start .. '\0]'
@@ -17,3 +17,44 @@ return function (content, title_start, template_start)
 		end
 	end)
 end
+
+require "mediawiki"
+
+local list_to_set = require "Module:table".listToSet
+
+-- Templates in which the language code is in the first parameter
+-- and the link target in the second.
+local link_template_names = list_to_set {
+	"m", "l", "t", "t+", "cog", "cognate", "ncog", "noncognate"
+}
+
+-- Templates in which the language code is in the second parameter
+-- and the link target in the third.
+local derivation_template_names = list_to_set {
+	"der", "derived", "inh", "inherited", "bor", "borrowed",
+	"cal", "calq", "calque", "clq",
+}
+
+local function iterate_links(content, title_start, template_start, template_iterator)
+	local template_iterator = template_iterator
+		or iterate_templates(content, title_start, template_start)
+	
+	return function ()
+		for template, title in template_iterator do
+			local language_code, link_target
+			if link_template_names[template.name] then
+				language_code = template.parameters[1]
+				link_target = template.parameters[2]
+			elseif derivation_template_names[template.name] then
+				language_code = template.parameters[2]
+				link_target = template.parameters[3]
+			end
+			
+			if language_code and link_target then
+				return language_code, link_target, title, template
+			end
+		end
+	end
+end
+
+return { iterate_templates = iterate_templates, iterate_links = iterate_links }
