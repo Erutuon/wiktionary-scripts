3f42baf5564c22d909ece62badbf755cb307b5b0
gbertilson <5840197+Erutuon@users.noreply.github.com>
2019-05-09 10:17:58 -0500

    add rule for language code

--- lua/link_template_sequences.lua
+++ lua/link_template_sequences.lua
@@ -39,27 +39,28 @@ local find_consecutive_link_templates = P {
 		* C(Cmt(V 'consecutive_link_templates', count_up)))^1,
 	-- (V 'comment' + C(V 'consecutive_link_templates') + 1)^1,
 	consecutive_link_templates = V 'start_link_template'
-		* Cmt(link_separator * Cb 'language_code' * V 'link_template' * Cb 'language_code',
+		* Cmt(link_separator * Cb 'link_language_code' * V 'link_template' * Cb 'link_language_code',
 			function (_, _, a, b)
 				return a == b
 			end)^1,
 	start_link_template = P '{{' * ws^0
 		* ((P 'm' * P 'ention'^-1) * ws^0
-			* P '|' * ws^0 * Cg((R 'az' + P '-')^2, 'language_code') * ws^0
+			* P '|' * ws^0 * Cg(V 'language_code', 'link_language_code') * ws^0
 		+ (P 'cog' * P 'nate'^-1
 		+  P 'n' * (P 'oncognate' + P 'cog')) * ws^0
-			* P '|' * ws^0 * Cg((R 'az' + P '-')^2 / get_non_etymological, 'language_code') * ws^0
+			* P '|' * ws^0 * Cg(V 'language_code' / get_non_etymological, 'link_language_code') * ws^0
 		+ (P 'der' * P 'ived'^-1
 		+  P 'bor' * P 'rowed'^-1
 		+  P 'inh' * P 'erited'^-1) * ws^0
-			* P '|' * ws^0 * (R 'az' + P '-')^2 * ws^0
-			* P '|' * ws^0 * Cg((R 'az' + P '-')^2 / get_non_etymological, 'language_code') * ws^0)
+			* P '|' * ws^0 * V 'language_code' * ws^0
+			* P '|' * ws^0 * Cg(V 'language_code' / get_non_etymological, 'link_language_code') * ws^0)
 		* (P '|' * ((1 - (P '|' + P '{{' + P '}}'))^1 + V 'template'))^0 * P '}}',
 	link_template = P '{{' * ws^0 * (P 'm' * P 'ention'^-1) * ws^0
-		* P '|' * ws^0 * Cg((R 'az' + P '-')^2, 'language_code') * ws^0
+		* P '|' * ws^0 * Cg(V 'language_code', 'link_language_code') * ws^0
 		* (P '|' * ((1 - (P '|' + P '{{' + P '}}'))^1 + V 'template'))^0 * P '}}',
 	template = P '{{' * (((1 - (P '{{' + P '}}'))^1 + V 'template'))^1 * P '}}',
 	comment = P '<!--' * (1 - P '-->')^0 * P '-->',
+	language_code = (R 'az' + P '-')^2,
 }
 
 local max = math.huge
